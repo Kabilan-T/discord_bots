5e0af2450f4e58fa99026ffa9252ba86d8dee657
Kabilan Tamilmani <kabilan.tamilmani@dfki.de>
2024-01-19 19:11:51 +0100

    added moderation commands

--- src/utils/moderation.py
+++ src/utils/moderation.py
@@ -9,192 +9,204 @@
 
 #-------------------------------------------------------------------------------
 
+import os
+import yaml
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
 
+
+# Default permissions : Change 
+PermissionToBan = discord.Permissions(ban_members=True)
+PermissionToWarn = discord.Permissions(ban_members=True)
+PermissionToKick = discord.Permissions(kick_members=True)
+PermissionToMute = discord.Permissions(mute_members=True)
+PermissionToDeafen = discord.Permissions(deafen_members=True)
+PermissionToPurge = discord.Permissions(manage_messages=True)
+PermissionBasic = discord.Permissions(send_messages=True)
+
+warn_list_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config", "warns.yml")
+
 class Moderation(commands.Cog, name="Moderation"):
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.hybrid_command( name="kick", description="Kick a user from the server.")
-    async def kick(self, context: Context, user: discord.Member, notify_user: bool = False, *, reason: str = None):
-        # Kick user
-        await user.kick(reason=reason)
-        embed = discord.Embed(
-            title="Kick",
-            description=f"{user.mention} has been kicked by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        embed.add_field(
-            name="Reason",
-            value=reason if reason is not None else "No reason provided.",
-            inline=False,
-        )
-        await context.send(embed=embed)
-        if notify_user:
+        self.warns = yaml.load(open(warn_list_file, "r"), Loader=yaml.FullLoader) if os.path.exists(warn_list_file) else dict()
+
+    def check(self, context: Context, permission):
+        # Check if the user has the required permissions
+        has_permissions = context.channel.permissions_for(context.author).is_superset(permission) or context.author.guild_permissions.is_superset(permission)
+        if not has_permissions:
             embed = discord.Embed(
-                title="Kick",
-                description=f"You have been kicked from {context.guild.name}",
+                title="Sorry :confused:",
+                description="You do not have permission to use this command. You need the following permission to use this command : " + str(permission),
                 color=0xBEBEFE,
             )
-            embed.add_field(
-                name="Reason",
-                value=reason if reason is not None else "No reason provided.",
-                inline=False,
-            )
-            await user.send(embed=embed)
-        self.bot.logger.info(f"{user} has been kicked by {context.author}")
+            context.send(embed=embed)
+            self.bot.logger.warning(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) tried to use a command. But they do not have the required permissions - {permission}")
+        return has_permissions
     
-    @commands.hybrid_command( name="ban", description="Ban a user from the server.")
-    async def ban(self, context: Context, user: discord.Member, notify_user: bool = False, *, reason: str = None):
-        # Ban user
-        await user.ban(reason=reason)
+    async def send_message(self, context: Context, member: discord.Member, action: str, reason: str = None, dm : bool = False):
         embed = discord.Embed(
-            title="Ban",
-            description=f"{user.mention} has been banned by {context.author.mention}.",
+            title="Moderation activity :judge:",
+            description=f"**{member.mention}** has been {action} by **{context.author.mention}**.",
             color=0xBEBEFE,
         )
-        embed.add_field(
-            name="Reason",
-            value=reason if reason is not None else "No reason provided.",
-            inline=False,
-        )
+        if reason is not None:
+            embed.add_field(name="Reason", value=reason, inline=False)
         await context.send(embed=embed)
-        if notify_user:
+        if dm:
+            await self.send_dm(context, member, action, reason)
+
+    async def send_dm(self, context: Context, member: discord.Member, action: str, reason: str = None):
+        embed = discord.Embed(
+            title="Moderation activity :judge:",
+            description=f"You have been {action} in **{context.guild.name}**",
+            color=0xBEBEFE,
+        )
+        if reason is not None:
+            embed.add_field(name="Reason", value=reason, inline=False)
+        try:
+            await member.send(embed=embed)
+        except discord.Forbidden:
+            self.bot.logger.warning(f"Could not send DM to {member.name}#{member.discriminator} ({member.id}) about {action} activity in {context.guild.name} ({context.guild.id})")
+            pass
+        
+    @commands.hybrid_command( name="ban", description="Ban a member from the server.")
+    async def ban(self, context: Context, member: discord.Member, reason: str = None):
+        # Ban a member from the server
+        if self.check(context, PermissionToBan):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) banned {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "banned :no_entry:", reason, False)
+            await member.ban(reason=reason)
+    
+    @commands.hybrid_command( name="unban", description="Unban a member from the server.")
+    async def unban(self, context: Context, member: discord.Member, reason: str = None):
+        # Unban a member from the server
+        if self.check(context, PermissionToBan):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) unbanned {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "unbanned :unlock:", reason, False)
+            await member.unban(reason=reason)
+
+    @commands.hybrid_command( name="kick", description="Kick a member from the server.")
+    async def kick(self, context: Context, member: discord.Member, reason: str = None):
+        # Kick a member from the server
+        if self.check(context, PermissionToKick):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) kicked {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "kicked :boxing_glove:", reason, False)
+            await member.kick(reason=reason)
+    
+    @commands.hybrid_command( name="mute", description="Mute a member in the server.")
+    async def mute(self, context: Context, member: discord.Member, reason: str = None):
+        # Mute a member in the server
+        if self.check(context, PermissionToMute):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) muted {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "muted :shushing_face:", reason, False)
+            await member.edit(mute=True, reason=reason)
+
+    @commands.hybrid_command( name="unmute", description="Unmute a member in the server.")
+    async def unmute(self, context: Context, member: discord.Member, reason: str = None):
+        # Unmute a member in the server
+        if self.check(context, PermissionToMute):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) unmuted {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "unmuted :microphone2:", reason, False)
+            await member.edit(mute=False, reason=reason)
+    
+    @commands.hybrid_command( name="deafen", description="Deafen a member in the server.")
+    async def deafen(self, context: Context, member: discord.Member, reason: str = None):
+        # Deafen a member in the server
+        if self.check(context, PermissionToDeafen):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) deafened {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "deafened :mute:", reason, False)
+            await member.edit(deafen=True, reason=reason)
+    
+    @commands.hybrid_command( name="undeafen", description="Undeafen a member in the server.")
+    async def undeafen(self, context: Context, member: discord.Member, reason: str = None):
+        # Undeafen a member in the server
+        if self.check(context, PermissionToDeafen):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) undeafened {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "undeafened :loud_sound:", reason, False)
+            await member.edit(deafen=False, reason=reason)
+    
+    @commands.hybrid_command( name="purge", description="Purge messages from a channel.")
+    async def purge(self, context: Context, amount: int):
+        # Purge messages from a channel
+        if self.check(context, PermissionToPurge):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) purged {amount} messages from {context.channel.name} ({context.channel.id}) in {context.guild.name} ({context.guild.id})")
+            await context.channel.purge(limit=amount)
             embed = discord.Embed(
-                title="Ban",
-                description=f"You have been banned from {context.guild.name}",
+                title="Purge :wastebasket:",
+                description=f"{amount} messages have been purged from {context.channel.mention}",
                 color=0xBEBEFE,
             )
-            embed.add_field(
-                name="Reason",
-                value=reason if reason is not None else "No reason provided.",
-                inline=False,
-            )
-            await user.send(embed=embed)
-        self.bot.logger.info(f"{user} has been banned by {context.author}")
+            await context.send(embed=embed)
     
-    @commands.hybrid_command( name="unban", description="Unban a user from the server.")
-    async def unban(self, context: Context, user: discord.User, notify_user: bool = False, *, reason: str = None):
-        # Unban user
-        await context.guild.unban(user)
-        embed = discord.Embed(
-            title="Unban",
-            description=f"{user.mention} has been unbanned by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        embed.add_field(
-            name="Reason",
-            value=reason if reason is not None else "No reason provided.",
-            inline=False,
-        )
-        await context.send(embed=embed)
-        if notify_user:
-            embed = discord.Embed(
-                title="Unban",
-                description=f"You have been unbanned from {context.guild.name}",
-                color=0xBEBEFE,
-            )
-            embed.add_field(
-                name="Reason",
-                value=reason if reason is not None else "No reason provided.",
-                inline=False,
-            )
-            await user.send(embed=embed)
-        self.bot.logger.info(f"{user} has been unbanned by {context.author}")
+    @commands.hybrid_command( name="warn", description="Warn a member in the server.")
+    async def warn(self, context: Context, member: discord.Member, reason: str = None):
+        # Warn a member in the server
+        if self.check(context, PermissionToWarn):
+            self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) warned {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+            await self.send_message(context, member, "warned :memo:", reason, True)
+            if member.id in self.warns.keys():
+                self.warns[member.id] += 1
+            else:
+                self.warns[member.id] = 1
+        # save the warns
+        with open(warn_list_file, "w+") as file:
+            yaml.dump(self.warns, file)
+
     
-    @commands.hybrid_command( name="purge", description="Delete messages from a channel.")
-    async def purge(self, context: Context, limit: int):
-        # Purge messages
-        await context.channel.purge(limit=limit)
-        embed = discord.Embed(
-            title="Purge",
-            description=f"{limit} messages have been deleted.",
-            color=0xBEBEFE,
-        )
-        await context.send(embed=embed)
-        self.bot.logger.info(f"{limit} messages have been deleted in {context.channel.name} of {context.guild.name} by {context.author}")
+    @commands.hybrid_command( name="removewarn", description="Remove a certain number of warns of a member in the server.")
+    async def removewarn(self, context: Context, member: discord.Member, amount: int):
+        # Remove a certain number of warns of a member in the server
+        if self.check(context, PermissionToWarn):
+            if member.id in self.warns.keys():
+                if self.warns[member.id] >= amount:
+                    self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) removed {amount} warns of {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+                    await self.send_message(context, member, "removed of " + str(amount) + " warns :arrow_down:", None, True)
+                    self.warns[member.id] -= amount
+                    with open(warn_list_file, "w+") as file:
+                        yaml.dump(self.warns, file)
+                else:
+                    self.clearwarns(context, member)
+            
+
+    @commands.hybrid_command( name="clearwarns", description="Clear all warns of a member in the server.")
+    async def clearwarns(self, context: Context, member: discord.Member):
+        # Clear all warns of a member in the server
+        if self.check(context, PermissionToWarn):
+            if member.id in self.warns.keys():
+                self.bot.logger.info(f"{context.author.name}#{context.author.discriminator} ({context.author.id}) cleared all warns of {member.name}#{member.discriminator} ({member.id}) from {context.guild.name} ({context.guild.id})")
+                await self.send_message(context, member, "cleared of all warns :white_check_mark:", None, False)
+                self.warns[member.id] = 0
+                with open(warn_list_file, "w+") as file:
+                    yaml.dump(self.warns, file)
+            else:
+                embed = discord.Embed(
+                    title="Warns :confused:",
+                    description=f"{member.mention} has no warns.",
+                    color=0xBEBEFE,
+                )
+                await context.send(embed=embed)
+            
+        
+    @commands.hybrid_command( name="warns", description="Get the number of warns a member has.")
+    async def warns(self, context: Context, member: discord.Member):
+        # Get the number of warns a member has
+        if self.check(context, PermissionBasic):
+            if member.id in self.warns.keys():
+                embed = discord.Embed(
+                    title="Warns :warning:",
+                    description=f"{member.mention} has {self.warns[member.id]} warns.",
+                    color=0xBEBEFE,
+                )
+            else:
+                embed = discord.Embed(
+                    title="Warns :warning:",
+                    description=f"{member.mention} has 0 warns.",
+                    color=0xBEBEFE,
+                )
+            await context.send(embed=embed)
     
-    @commands.hybrid_command( name="mute", description="Mute a user.")
-    async def mute(self, context: Context, user: discord.Member):
-        # Mute user
-        role = discord.utils.get(context.guild.roles, name="Muted")
-        await user.add_roles(role)
-        embed = discord.Embed(
-            title="Mute",
-            description=f"{user.mention} has been muted by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        await context.send(embed=embed)
-        self.bot.logger.info(f"{user} has been muted by {context.author}")
-
-    @commands.hybrid_command( name="unmute", description="Unmute a user.")
-    async def unmute(self, context: Context, user: discord.Member):
-        # Unmute user
-        role = discord.utils.get(context.guild.roles, name="Muted")
-        await user.remove_roles(role)
-        embed = discord.Embed(
-            title="Unmute",
-            description=f"{user.mention} has been unmuted by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        await context.send(embed=embed)
-        self.bot.logger.info(f"{user} has been unmuted by {context.author}")
-
-    @commands.hybrid_command( name="warn", description="Warn a user.")
-    async def warn(self, context: Context, user: discord.Member, notify_user: bool = False, *, reason: str = None):
-        # Warn user
-        # todo: ------> add a warn to the user : user.warns += 1
-        embed = discord.Embed(
-            title="Warn",
-            description=f"{user.mention} has been warned by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        embed.add_field(
-            name="Reason",
-            value=reason if reason is not None else "No reason provided.",
-            inline=False,
-        )
-        await context.send(embed=embed)
-        if notify_user:
-            embed = discord.Embed(
-                title="Warn",
-                description=f"You have been warned in {context.guild.name}",
-                color=0xBEBEFE,
-            )
-            embed.add_field(
-                name="Reason",
-                value=reason if reason is not None else "No reason provided.",
-                inline=False,
-            )
-            await user.send(embed=embed)
-        self.bot.logger.info(f"{user} has been warned by {context.author}")
-
-    @commands.hybrid_command(name="warns", description="Get the number of warns for a user.")
-    async def warns(self, context: Context, user: discord.Member):
-        # Get warns for user
-        # todo: ------> get the number of warns for the user : user.
-        warns = 0
-        embed = discord.Embed(
-            title="Warns",
-            description=f"{user.mention} has {warns} warns.",
-            color=0xBEBEFE,
-        )
-        await context.send(embed=embed)
-        self.bot.logger.info(f"{user} has {warns} warns.")
-
-    @commands.hybrid_command( name="clearwarns", description="Clear the warns for a user.")
-    async def clearwarns(self, context: Context, user: discord.Member):
-        # Clear warns for user
-        # todo: ------> clear the warns for the user : user.warns = 0
-        embed = discord.Embed(
-            title="Clear Warns",
-            description=f"{user.mention} warns have been cleared by {context.author.mention}.",
-            color=0xBEBEFE,
-        )
-        await context.send(embed=embed)
-        self.bot.logger.info(f"{user} warns have been cleared by {context.author}")
-
+                 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
