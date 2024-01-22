572630390164b921efae587a2e1602c0a92f6335
Kabilan Tamilmani <kabilan.tamilmani@dfki.de>
2024-01-22 19:24:43 +0100

    greeting messages fixed

--- src/utils/greetings.py
+++ src/utils/greetings.py
@@ -21,44 +21,106 @@ class Greetings(commands.Cog, name="Greetings"):
     async def on_member_join(self, member: discord.Member):
         ''' Send welcome message when a new member joins the server '''
         guild = member.guild
-
-        title = f"Welcome to {guild.name} passenger {member.name}!"
-        description = f"Axiom is now your cosmic home. Join in, connect, and enjoy the journey! :ringed_planet: \n\n"
-        description += f" :flying_saucer::Passenger {member.mention} has boarded the ship!"
-        description += f" :passport_control: Passenger count: {guild.member_count}"
-        description += f" :rocket: Buckle up and Blast off!"                                                                           
-        embed = discord.Embed(title=title,
-                              description=description,
-                              color=discord.Color.green())
-        embed.set_author(name=self.bot.name+", The Autopilot",
-                            icon_url=self.bot.user.avatar_url)
-        embed.set_thumbnail(url=member.avatar_url)
-        embed.set_image(url="https://y.yarn.co/9e28c966-6728-47b8-9d39-8f0351b6e54a_text.gif")
-
+        if guild.id != 1182777669373399202:  # Axiom guild id 
+            return
+        self.bot.logger.info(f"New member {member.display_name} joined {guild.name}")
         if guild.system_channel is not None:
+            embed = await self.get_welcome_message(guild, member)
             await guild.system_channel.send(embed=embed)
-
+            self.bot.logger.info(f"Sending greeting message to {member.display_name} in {guild.system_channel.name}")
+        try:
+            embed = await self.get_welcome_dm_message(guild, member)
+            await member.send(embed=embed)
+            self.bot.logger.info(f"Sending greeting message to {member.display_name} in DM")
+        except discord.Forbidden:
+            pass
 
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         ''' Send goodbye message when a member leaves the server '''
         guild = member.guild
-
-        title = f"Goodbye passenger {member.name}!"
-        description = f" :flying_saucer: Passenger {member.mention} has left the ship!"
-        description += f" :passport_control: Passenger count: {guild.member_count}"
-        description += f" :rocket: Hope you enjoyed your stay!"
-        embed = discord.Embed(title=title,
-                              description=description,
-                              color=discord.Color.red())
-        embed.set_author(name=self.bot.name+", The Autopilot",
-                            icon_url=self.bot.user.avatar_url)
-        embed.set_thumbnail(url=member.avatar_url)
-        
+        if guild.id != 1182777669373399202:
+            return
+        self.bot.logger.info(f"Member {member.display_name} left {guild.name}")
         if guild.system_channel is not None:
+            embed = await self.get_goodbye_message(guild, member)
             await guild.system_channel.send(embed=embed)
-            
+            self.bot.logger.info(f"Sending goodbye message to {member.display_name} in {guild.system_channel.name}")
+        try:
+            embed = await self.get_goodbye_dm_message(guild, member)
+            await member.send(embed=embed)
+            self.bot.logger.info(f"Sending goodbye message to {member.display_name} in DM")
+        except discord.Forbidden:
+            pass
+
+    async def get_welcome_message(self, guild: discord.Guild,  member: discord.Member):
+        ''' Get welcome message for a member '''
+
+        embed = discord.Embed()
+        embed.set_author(name=self.bot.name+", The Autopilot",
+                         icon_url=self.bot.user.avatar.url)
+        embed.set_thumbnail(url=member.avatar.url)
+        embed.title = f"Welcome to {guild.name} passenger {member.display_name}!"
+        embed.description = f"Axiom is now your cosmic home. Join in, connect, and enjoy the journey! :ringed_planet: \n\n"
+        embed.description += f" :flying_saucer: Passenger {member.mention} has boarded the ship! \n"
+        embed.description += f" :rocket: Buckle up and Blast off!"
+        embed.color = discord.Color.green()
+        embed.add_field(name= f" :passport_control: Passenger count:",
+                        value=f"{guild.member_count}",
+                        inline=True)
+        embed.add_field(name= f" :crystal_ball: Star Date:",
+                        value=f"{(discord.utils.utcnow() - guild.created_at).days} days",
+                        inline=True)
+        embed.set_image(url="https://y.yarn.co/9e28c966-6728-47b8-9d39-8f0351b6e54a_text.gif")
+        embed.set_footer(text=f"Your journey begins here @{member.name}")
+        return embed
+    
+    async def get_goodbye_message(self, guild: discord.Guild,  member: discord.Member):
+        ''' Get goodbye message for a member '''
+
+        embed = discord.Embed()
+        embed.set_author(name=self.bot.name+", The Autopilot",
+                            icon_url=self.bot.user.avatar.url)
+        embed.set_thumbnail(url=member.avatar.url)
+        embed.title = f"Goodbye passenger {member.display_name}!"
+        embed.description = f" :flying_saucer: Passenger {member.mention} has left the ship! \n"
+        embed.description += f" :rocket: Hope you enjoyed your stay!"
+        embed.color = discord.Color.red()
+        embed.add_field(name= f" :passport_control: Passenger count:",
+                        value=f"{guild.member_count}",
+                        inline=True)
+        embed.add_field(name= f" :crystal_ball: Journey Duration:",
+                        value=f"{(discord.utils.utcnow() - member.joined_at).days} days",
+                        inline=True)
+        
+        embed.set_footer(text=f"We will miss you @{member.name}")
+        return embed
+    
+    async def get_welcome_dm_message(self, guild: discord.Guild,  member: discord.Member):
+        ''' Get welcome message for a member '''
+        embed = discord.Embed()
+        embed.set_author(name=self.bot.name+", The Autopilot",
+                         icon_url=self.bot.user.avatar.url)
+        embed.set_thumbnail(url=member.avatar.url)
+        embed.title = f"You have arrived at {guild.name} passenger {member.display_name}!"
+        embed.description = f"Axiom is our cosmic home. There are {guild.member_count} passengers on board in {guild.name}. \n\n"
+        embed.description += f" We welcome you with open arms and hope you enjoy your stay! \n"
+        embed.description += f" :rocket: Buckle up and Blast off!"
+        embed.color = discord.Color.green()
+        return embed
+
+    async def get_goodbye_dm_message(self, guild: discord.Guild,  member: discord.Member):
+        ''' Get goodbye message for a member '''
+        embed = discord.Embed()
+        embed.set_author(name=self.bot.name+", The Autopilot",
+                            icon_url=self.bot.user.avatar.url)
+        embed.set_thumbnail(url=member.avatar.url)
+        embed.title = f"Goodbye passenger {member.display_name}!"
+        embed.description = f"We are sorry to see you leave {guild.name}. \n\n"
+        embed.description += f" :rocket: Hope you enjoyed your stay!"
+        embed.color = discord.Color.red()
+        return embed
 
 async def setup(bot):
-    bot.add_cog(Greetings(bot))
+    await bot.add_cog(Greetings(bot))
