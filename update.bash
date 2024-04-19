89565adc93429996c4d2e40d94a6bf524f57a88a
Kabilan Tamilmani <kabilan.tamilmani@dfki.de>
2024-04-19 11:41:26 +0200

    minor change in update.bash

--- update.bash
+++ update.bash
@@ -2,20 +2,24 @@
 
 # Change directory to the location of the script
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$SCRIPTPATH"
+cd "$SCRIPTPATH" || exit
 
-echo "$(date) ------ Updating the repository ------"
+echo "--------------------------------------------------"
+echo "Time - $(date)"
+
 # Pull recent changes from the remote repository
+echo "----- Updating repository -----"
+PULL_OUTPUT=$(git pull origin main)
 
-git pull origin main
-
-# Check if there are any changes
-if [ $? -eq 0 ]; then
-    echo "Changes detected, running setup.bash"
-    # Run setup.bash
-    bash setup.bash
+# Check if there were actual changes pulled
+if [[ $PULL_OUTPUT == *"Already up to date."* ]]; then
+    echo "--- No changes detected ---"
 else
-    echo "No changes detected, exiting"
+    echo "--- Changes detected ---"
+    # Run setup.bash
+    echo "---------- Running Setup ----------"
+    bash setup.bash
+    echo "---------- Setup is done ----------"
 fi
-# add a empty line
+
 echo "--------------------------------------------------"
