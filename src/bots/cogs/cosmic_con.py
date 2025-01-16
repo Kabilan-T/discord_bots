597c3821d4977b4283d885804a6c8e856b96d158
Kabilan Tamilmani <kabilan.tamilmani@dfki.de>
2025-01-16 15:02:08 +0100

    added more command

--- src/bots/cogs/cosmic_con.py
+++ src/bots/cogs/cosmic_con.py
@@ -30,7 +30,22 @@ class CosmicCon(commands.Cog):
         self.taunt = dict()
         self.taunt_gifs = ["https://tenor.com/view/willem-dafoe-laugh-crazy-car-gif-22580177",
                             "https://tenor.com/view/krillin-taunt-taunting-anime-dbz-gif-13298362",
-                            "https://tenor.com/view/loafcat-meme-crypto-boxing-dance-gif-14735673580763849381"]
+                            "https://tenor.com/view/loafcat-meme-crypto-boxing-dance-gif-14735673580763849381",
+                            "https://tenor.com/view/pithon-taunt-gif-12865791201436561590",
+                            "https://tenor.com/view/pokemon-squirtle-taunt-gif-17959902",
+                            "https://tenor.com/view/blegh-belt-the-croods-funny-face-tease-gif-19072929",
+                            "https://tenor.com/view/jmthjk-gif-22224450",
+                            "https://tenor.com/view/funny-silly-lion-king-gif-14287886"]
+        
+        self.foxy = dict()
+        self.foxy_gifs = ["https://tenor.com/view/heypillagada-saipallavi-kali-gif-21578869"
+                            "https://tenor.com/view/sarange-don-sivakarthikeyan-priyanka-mohan-gif-25621599"
+                            "https://tenor.com/view/vinnaithandi-varuvaya-vtv-anbil-avan-trisha-jessie-gif-17836131"
+                            "https://tenor.com/view/romantic-trending-neethaane-en-ponvasantham-samantha-akkineni-love-gif-7747061558216435844"
+                            "https://tenor.com/view/meenakshi-chaudhary-bhibatsam-beautiful-angry-annoyed-gif-2262741861409338122"
+                            "https://tenor.com/view/tara-in-okk-ok-bangaram-ok-kanmani-maniratnam-dulquer-gif-14768119681824719686"
+                            "https://tenor.com/view/mrunal-thakur-hi-nanna-cute-smile-smirk-gif-4586904126258729553"
+                            "https://tenor.com/view/janhvi-janhvi-kapoor-vogue-vogue-bffs-wink-gif-20008230"]
 
 
     ## Star Wars reference
@@ -211,15 +226,49 @@ class CosmicCon(commands.Cog):
         await context.send(embed=embed)
         self.bot.log.info(f"Shinobi {member.name} has been trapped in Tsukuyomi by {context.author.name} in {context.guild.name}", context.guild)
 
+     ## Game cheat code reference
+    @commands.command(name='foxy_magnet', description = "Atractive level 100")
+    async def foxy_magnet(self, context: Context, member: discord.Member):
+        ''' Send cute gifs to the user for 24 hours whenever they send a message '''
+        if context.author.guild_permissions.moderate_members:
+            embed = discord.Embed(title="Foxy Magnet",
+                                description="You need to be a pro gamer to use this cheat code",
+                                color=self.bot.default_color)
+            await context.send(embed=embed)
+            return
+        self.foxy[member.id] = datetime.datetime.now(tz=datetime.timezone.utc)+datetime.timedelta(hours=24)
+        embed = discord.Embed(title="Foxy Magnet",
+                            description=f"{member.mention} has been surrounded by gifs for next 24 hours",
+                            color=self.bot.default_color)
+        await context.send(embed=embed)
+        self.bot.log.info(f"Player {member.name} has been surrounded by gifs by {context.author.name} in {context.guild.name}", context.guild)
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         ''' Check if the user is taunted '''
         if message.author.id in self.taunt:
             if datetime.datetime.now(tz=datetime.timezone.utc) < self.taunt[message.author.id]:
                 gif = random.choice(self.taunt_gifs)
-                await message.reply(f"{message.author.mention} {gif}")
+                await message.reply(f"{gif}")
             else:
                 self.taunt.pop(message.author.id, None)
+                embed = discord.Embed(title="Tsukuyomi",
+                                    description=f"{message.author.mention} has been released from the Tsukuyomi",
+                                    color=self.bot.default_color)
+                await message.channel.send(embed=embed)
+                self.bot.log.info(f"Shinobi {message.author.name} has been released from Tsukuyomi", message.guild)
+
+        if message.author.id in self.foxy:
+            if datetime.datetime.now(tz=datetime.timezone.utc) < self.foxy[message.author.id]:
+                gif = random.choice(self.foxy_gifs)
+                await message.reply(f"{gif}")
+            else:
+                self.foxy.pop(message.author.id, None)
+                embed = discord.Embed(title="Foxy Magnet",
+                                    description=f"{message.author.mention} has been released from the Foxy Magnet",
+                                    color=self.bot.default_color)
+                await message.channel.send(embed=embed)
+                self.bot.log.info(f"Player {message.author.name} has been released from Foxy Magnet", message.guild)
 
     ## R2D2 reference
     @commands.command(name='r2d2', description = "Translate a message to R2D2's language")
